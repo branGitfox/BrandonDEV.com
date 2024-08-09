840b804c8fa0f0073debf7b1f3a1cf77be0c7926
BranGitFox <vixfgit@gmail.com>
2024-08-09 22:39:10 +0200

    finishing tutorial and starting contact page

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Nav from './components/nav/Nav.jsx'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
