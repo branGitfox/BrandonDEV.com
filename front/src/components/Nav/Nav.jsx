fa35154e0cc73530496a1c09c99fb744f66725ba
Brandon Fidelin  Ravomanana <135463036+branGitfox@users.noreply.github.com>
2024-08-28 08:29:19 +0300

    Update Nav.jsx

--- front/src/components/Nav/Nav.jsx
+++ front/src/components/Nav/Nav.jsx
@@ -41,7 +41,7 @@ function Nav() {
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex flex-shrink-0 items-center">
-              <h3 className='font-medium text-3xl text-blue-700'>BR.com</h3>
+              <h3 className='font-medium font-bold text-4xl text-blue-700'>BR.com</h3>
             </div>
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
