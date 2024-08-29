4187d911656cf3ae39309f8e8be1bd4e6235116b
Brandon Fidelin  Ravomanana <135463036+branGitfox@users.noreply.github.com>
2024-08-29 08:08:33 +0300

    Update NotFound.jsx

--- front/src/components/404/NotFound.jsx
+++ front/src/components/404/NotFound.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import Nav from '../../Nav/Nav'
 function NotFound() {
   return (
+    <Nav/>
     <div className='mt-5 '>
         <h1 className='text-red-400 font-bold text-4xl' >404 Not Found</h1>
         <p className='text-orange-400 font-base' >*la page que vous essayer d'acceder n'existe pas </p>
