29e197cd628246cb0daacf99b1160f205869411b
Brandon Fidelin  Ravomanana <135463036+branGitfox@users.noreply.github.com>
2024-08-30 09:39:55 +0300

    Update Signin.jsx

--- front/src/components/Admin/Signin/Signin.jsx
+++ front/src/components/Admin/Signin/Signin.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import {toast, ToastContainer} from 'react-toastify'
 import Nav from '../../Nav/Nav'
 import Form from '../Form/Form'
