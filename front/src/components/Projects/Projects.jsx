37934afd0a6e04e7a8b684657aedb1ba1483c9ad
Brandon Fidelin  Ravomanana <135463036+branGitfox@users.noreply.github.com>
2024-09-01 11:42:37 +0300

    Update Projects.jsx

--- front/src/components/Projects/Projects.jsx
+++ front/src/components/Projects/Projects.jsx
@@ -39,15 +39,15 @@ function Project({projects}) {
         <>
         {
             projects?.map((project, index) => (
-                <div key={index} className="w-96 h rounded border-none  shadow shadow-blue-700 ">
+                <div key={index} className="w-96 h-auto rounded border-none  shadow shadow-blue-700 ">
                     <div className=" relative w-2/2 h-2/4 border-b-2 border-gray-800 border-solid">
                         <img src={project?.image}  className='w-full h-full rounded-t-md rounded-b-none'/>
                     </div>
-                    <div className=" w-full  p-2">
+                    <div className=" w-full  h-auto p-2">
                         <h4 className="text-gray-500"><InformationCircleIcon className='w-7 h-7 inline text-violet-500'/> {project?.name}</h4>
                         <p className="text-gray-400  mx-2 mt-3 mb-3 font-light">{project?.description}</p>
                         <h4 className="text-gray-500"><ClipboardDocumentListIcon className='w-7 h-7 inline text-violet-500'/> Stacks</h4>
-                        <p className="text-gray-400 font-light mx-2 mt-3 mb-3">{project?.stacks}</p>
+                        <p className="text-gray-400 font-light mx-2 mt-3 mb-1">{project?.stacks}</p>
                         <div className="w-full flex justify-around p-2 mt-2">
                                 <a href={project?.site} className='w-20 rounded border-2 text-white justify-center  flex items-center border-gray-500 border-solid'>Live<WifiIcon className='w-5 h-5 text-blue-500 inline mx-1'/></a>
                                <a href={project?.source} className='w-20 rounded border-2 text-white justify-center  flex items-center border-gray-500 border-solid'>Code<CommandLineIcon className='w-5  h-5 text-green-500 inline mx-1'/></a>     
